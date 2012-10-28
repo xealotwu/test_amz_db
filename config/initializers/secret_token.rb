e9b5beafb200c15d5484a22ae9377e7983623ff3
Di Wu <di@ubuntu.(none)>
2012-10-28 15:40:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAmzDb::Application.config.secret_token = '1ab742ef7639f77bf50dbc3034b1d5a4ad1ffcf0482de855aef72b55465c19426b0fdd5f77fc57d388b3a64b53a1e50d971f4d35c8573d1a951a5d0b1ad6a02e'
